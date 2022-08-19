0bc47058d1501c350ef4a356c7a2b7a27ff66c34
Mira Mansour <81388088+m-mansour1@users.noreply.github.com>
2022-08-19 20:48:22 +0300

    Create sentiment_analysis.py

---
+++ sentiment_analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import text_normalizer as tn
+# import model_evaluation_utils as meu  ## Not provided to you
+# The Intern can code the methods to print the Metric like accuracy,
+# and confusion matrix. The o/ps in the below cells would give an idea.
+
+np.set_printoptions(precision=2, linewidth=80)
